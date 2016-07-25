ce34993203b87d567ba9007daf67011910ba6b0a
marinacor1 <marinaspam1234@gmail.com>
2016-07-25 12:31:55 -0600

    User login spec pathing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
+      redirect_to links_path
     else
       flash[:alert] = "Incorrect email/ password combination."
       redirect_to login_path
