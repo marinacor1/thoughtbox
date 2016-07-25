9ab8ba89ed21dd5b8084088c838321fbf15c44aa
marinacor1 <marinaspam1234@gmail.com>
2016-07-25 10:43:54 -0600

    Links model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725155134) do
+ActiveRecord::Schema.define(version: 20160725164232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string  "title"
+    t.string  "url"
+    t.boolean "uread", default: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
