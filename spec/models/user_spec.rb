f15340b49751facbb6d86d5dcfa20ba285dc54eb
marinacor1 <marinaspam1234@gmail.com>
2016-07-25 10:52:15 -0600

    Model tests for user are all passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "checks to make sure email addresses are not the same" do
+     existing_user = User.create(email: "email@email.com", password: "password", password_confirmation: "password")
+
+     new_user = User.create(email: "email@email.com", password: "password", password_confirmation: "password")
+
+     expect(User.count).to eq(1)
+  end
+
+  it "checks to make sure all information is complete" do
+     User.create(email: "email@email.com", password: "password")
+
+    expect(User.count).to eq(0)
+  end
+
+  it "checks to make sure password and password confirmation are same" do
+    User.create(email: "email@email.com", password: "password", password_confirmation: "password")
+
+    expect(User.count).to eq(1)
+  end
+
+  it "will not save if password and password confirmation not matching" do
+    User.create(email: "email@email.com", password: "pord", password_confirmation: "password")
+
+    expect(User.count).to eq(0)
+
+  end
 end
