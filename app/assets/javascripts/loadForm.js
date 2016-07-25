1904c0786b76a8539d1761df032d1e1d21c4e08e
marinacor1 <marinaspam1234@gmail.com>
2016-07-25 14:02:40 -0600

    Form appears

---
+++ app/assets/javascripts/loadForm.js
@@ -0,0 +1,14 @@
+function loadForm(selector){
+  var $formTarget = $(selector)
+  $formTarget.append(formatForm());
+}
+
+ function formatForm(){
+  return '<div id="link-form">' +
+          '<form>Title <br>' +
+'<input type="text" name="link-title" class="link-title"><br>' +
+'Url: <br>' + '<input type="text" name="link-url" class="link-url"><br>' +
+'<input type="submit" value="Submit">'
++        '</form>' +
+      '</div>'
+}
