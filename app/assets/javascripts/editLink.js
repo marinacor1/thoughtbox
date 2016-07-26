9377302b95b095e12b1e7a20d535c3bbf3c51781
marinacor1 <marinaspam1234@gmail.com>
2016-07-26 10:08:46 -0600

    edit link not hitting api

--- app/assets/javascripts/editLink.js
+++ app/assets/javascripts/editLink.js
@@ -1,13 +1,30 @@
 function editLink(selector){
   $(selector).delegate('.edit-button', 'click', function(event){
     var currentId = event.target.parentElement.outerHTML.split("=")[1].split(" ")[1];
-    console.log('edit button is hit in div ' + currentId);
-    $('#link-subset').append(newFormBox);
+    $('#link-subset').append(newFormBox(currentId));
   });
 
-  function newFormBox(){
-    return '<br>New Title: <input type="text" name="inputform"</input>' +
-    '<br>New Url: ' + '<input type="text" name="urlform"</input><br>' +
-    '<button type="button" class="edit-submit-button">Submit</button>';
+  function newFormBox(currentId){
+    return '<br> New Title: <input type="text" class="new-title" name="inputform"</input>' +
+    '<br>New Url: ' + '<input type="text" class="new-url" name="urlform"</input><br>' +
+    '<button type="button" onclick="updateData(' + currentId + ')" class="edit-submit-button">Submit</button>';
   }
+}
+
+function updateData(currentId){
+  console.log("current id is " + currentId);
+  var newTitle = $('.new-title').val();
+  var newUrl = $('.new-url').val();
+  var newData = {title: newTitle, url: newUrl};
+  $.ajax({
+    type: "PATCH",
+    url: "api/v1/links/" + currentId,
+    data: newData,
+    datType: 'json',
+    success: noReloadLink(currentId)
+  });
+}
+
+function noReloadLink(currentId){
+
 }
