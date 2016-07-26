dd47cf538b72e5ed7fed5d1cdf15d2340ce61bec
marinacor1 <marinaspam1234@gmail.com>
2016-07-26 09:19:00 -0600

    Will changes status but only after reload

--- app/assets/javascripts/update.js
+++ app/assets/javascripts/update.js
@@ -1,3 +1,40 @@
 function updateReadStatus(selector){
+  $(selector).delegate('#unread-check', 'click', function(event){
+    console.log("unread check was selected");
+    var currentId = event.target.parentElement.outerHTML.split(" ")[0].split('<ul').pop().split(">")[0];
+    updateStatus( {unread: false}, {id: currentId });
+  });
+
+  $(selector).delegate('#read-check', 'click', function(){
+    console.log("read check was selected");
+    var currentId = event.target.parentElement.outerHTML.split(" ")[0].split('<ul').pop().split(">")[0];
+    updateStatus( {unread: true}, {id: currentId});
+  });
 
 }
+
+function updateStatus (newData, currentId) {
+  $.ajax({
+    type: "PATCH",
+    url: "api/v1/links/" + currentId.id,
+    data: newData,
+    dataType: 'json',
+    success: noReloadStatus(newData, currentId),
+  });
+}
+
+function noReloadStatus(newData, currentId){
+  $(".checkbox").prop("checked", false);
+  $('#link-subset' + currentId.id).replaceWith(formatUnread(newData));
+}
+
+  function formatUnread(link){
+    if (link.unread){
+      console.log("link is now unread");
+      return '<div id="unread-check">' +'<ul> Mark as Read: <input type="checkbox" class="checkbox-unread"></ul></div>';
+  }
+    else {
+      console.log("link is now read");
+    return '<div id="read-check">' + '<ul>Mark as Unread: <input type="checkbox" class="checkbox-read"></ul>';
+  }
+  }
