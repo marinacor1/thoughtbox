b5b1439e427ba52782b13e577113986b25c08b29
marinacor1 <marinaspam1234@gmail.com>
2016-07-29 21:57:02 -0600

    Alphabetical works though styling is off

--- app/assets/javascripts/sortAlphabetically.js
+++ app/assets/javascripts/sortAlphabetically.js
@@ -1,36 +1,43 @@
 function sortAlphabetically(selector){
   $(selector).click(function(){
   var $currentLinks = $('#link-table').children().sort();
-  var sorted = [];
-  var alphabetized = $currentLinks.each(function(index, individualLink){
-    // var title = individualLink.firstchild.firstChild.innerText
-    var linkInfo = individualLink.textContent.split("http");
-    linkInfo[1] = "http" + linkInfo[1];
-    sorted.push({content: linkInfo, id: index});
+  $.ajax({
+    type: "GET",
+    url: "api/v1/sorted",
+    success: renderLinks(selector)
   });
-  debugger
-  renderLinks(selector, sorted.sort());
+  // var sorted = [];
+  // var alphabetized = $currentLinks.each(function(index, individualLink){
+    // var title = individualLink.firstchild.firstChild.innerText
+  //   var linkInfo = individualLink.textContent.split("http");
+  //   linkInfo[1] = "http" + linkInfo[1];
+  //   sorted.push({content: linkInfo, id: index});
+  // });
+  // renderLinks(selector, sorted.sort(sorted.content));
   });
 }
 
-  function renderLinks(selector, sorted){
+  function renderLinks(selector){
+    return function(response){
       $('#link-table').empty();
-      $.each(sorted, function(index, link){
+      $.each(response, function(index, link){
         $('#link-table').append(formatLink(link));
       });
-  }
+  };
 
   function formatLink(link){
-    return '<div id="link-table"><ul id="title-box"' + link.id + '>'+ link.content[0] +
-    '</ul><ul id="url-box" ' + link.id + '>' + link.content[1].split(" ")[0] + '</ul>' + formatUnread(link) + '<button type="button" onclick="editLink()" class="edit-button">Edit</button>' +
-    '</div>';
+    return '<div id="link-table"><div id="link-subset"' + link.id +'<ul id="title-box"' + link.id + '>'+ link.title +
+    '</ul><ul id="url-box" ' + link.id + '>' + link.url + '</ul>' +
+    formatUnread(link) + '<button type="button" onclick="editLink()" class="edit-button">Edit</button>' +
+    '</div></div>';
 }
 
   function formatUnread(link){
-    if (link.content[1].split("")[3] === "Read:"){
+    if (link.unread){
     return '<div id="unread-check">' +'<ul' + link.id + '> Mark as Read: <input type="checkbox" class="checkbox-unread"></ul></div>';
   }
     else {
     return '<div id="read-check">' + '<ul' + link.id + '>Mark as Unread: <input type="checkbox" class="checkbox-read"></ul>';
   }
+}
 }
