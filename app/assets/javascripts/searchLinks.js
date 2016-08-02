cb72df6ee334de178ed146bcf239e3f9098490b3
marinacor1 <marinaspam1234@gmail.com>
2016-08-02 11:52:15 -0600

    Search is fixed

--- app/assets/javascripts/searchLinks.js
+++ app/assets/javascripts/searchLinks.js
@@ -1,7 +1,6 @@
 function searchLinks(selector){
   $(selector).keyup(function(event){
     var searchWords = event.key.toLowerCase();
-    console.log("Search words are " + searchWords)
       if (searchWords == "backspace") {
         searchWords = $(this).val();
       }
@@ -13,9 +12,9 @@ function loadSelectWords(searchWords){
   var $currentLinks = $('#link-table').children();
       console.log("current links are " + $currentLinks);
   var notRightLinks = $currentLinks.each(function(index, individualLink){
-    var id = 
-    debugger
-    var linkTitleAndUrl = $(individualLink).find('#title-box' + , '#url-box').text().toLowerCase();
+    var id = individualLink.firstChild.id.split("-")[2];
+    console.log("id is " + id);
+    var linkTitleAndUrl  = $(individualLink).find('#title-box' + id + '', '#url-box-' + id + '').text().toLowerCase();
     console.log("Var link title and url are" + linkTitleAndUrl);
     if (linkTitleAndUrl.includes(searchWords)){
       console.log('we have the right word')
