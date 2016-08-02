46a01d4974b8138031d5868d8be1a068f44ac255
marinacor1 <marinaspam1234@gmail.com>
2016-08-02 11:53:28 -0600

    Remove all console.logs

--- app/assets/javascripts/searchLinks.js
+++ app/assets/javascripts/searchLinks.js
@@ -10,20 +10,13 @@ function searchLinks(selector){
 
 function loadSelectWords(searchWords){
   var $currentLinks = $('#link-table').children();
-      console.log("current links are " + $currentLinks);
   var notRightLinks = $currentLinks.each(function(index, individualLink){
     var id = individualLink.firstChild.id.split("-")[2];
-    console.log("id is " + id);
     var linkTitleAndUrl  = $(individualLink).find('#title-box' + id + '', '#url-box-' + id + '').text().toLowerCase();
-    console.log("Var link title and url are" + linkTitleAndUrl);
     if (linkTitleAndUrl.includes(searchWords)){
-      console.log('we have the right word')
-      // $(individualLink).show();
       $(individualLink).removeClass('invisible');
     } else {
-      // $(individualLink).hide();
       $(individualLink).addClass('invisible');
-      console.log('we do not have the right word')
     }
   });
 }
