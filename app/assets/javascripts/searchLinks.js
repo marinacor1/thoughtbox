07fa5afd6c6f5e68d1f5d5c280b7b707b268b766
marinacor1 <marinaspam1234@gmail.com>
2016-08-02 11:39:52 -0600

    Search is broken

--- app/assets/javascripts/searchLinks.js
+++ app/assets/javascripts/searchLinks.js
@@ -1,6 +1,7 @@
 function searchLinks(selector){
   $(selector).keyup(function(event){
     var searchWords = event.key.toLowerCase();
+    console.log("Search words are " + searchWords)
       if (searchWords == "backspace") {
         searchWords = $(this).val();
       }
@@ -10,14 +11,20 @@ function searchLinks(selector){
 
 function loadSelectWords(searchWords){
   var $currentLinks = $('#link-table').children();
+      console.log("current links are " + $currentLinks);
   var notRightLinks = $currentLinks.each(function(index, individualLink){
-    var linkTitleAndUrl = $(individualLink).find('#title-box', '#url-box').text().toLowerCase();
+    var id = 
+    debugger
+    var linkTitleAndUrl = $(individualLink).find('#title-box' + , '#url-box').text().toLowerCase();
+    console.log("Var link title and url are" + linkTitleAndUrl);
     if (linkTitleAndUrl.includes(searchWords)){
+      console.log('we have the right word')
       // $(individualLink).show();
       $(individualLink).removeClass('invisible');
     } else {
       // $(individualLink).hide();
       $(individualLink).addClass('invisible');
+      console.log('we do not have the right word')
     }
   });
 }
